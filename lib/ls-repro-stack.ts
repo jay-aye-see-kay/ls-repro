85b19dfc5d3d9468712f00aae98dadd8fcc4e0ea
Jack Rose <user@jackrose.co.nz>
2022-07-29 15:03:13 +1000

    many things

--- lib/ls-repro-stack.ts
+++ lib/ls-repro-stack.ts
@@ -1,15 +1,27 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
+import { HitCounter } from "./hitcounter";
 
 export class LsReproStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LsReproQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWithCounter = new HitCounter(this, "HitCounter", {
+      downstream: hello,
+    });
+
+    new apigw.LambdaRestApi(this, "RawEndpoint", {
+      handler: hello,
+    });
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler,
+    });
   }
 }
