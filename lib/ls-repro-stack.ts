7409d0e4a57c296ec910fb5182bfd00ff2785a0b
Jack Rose <user@jackrose.co.nz>
2022-08-01 21:14:48 +1000

    create a hello world lambda

--- lib/ls-repro-stack.ts
+++ lib/ls-repro-stack.ts
@@ -1,15 +1,14 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class LsReproStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LsReproQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
   }
 }
