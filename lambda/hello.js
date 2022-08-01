9ac51ed75010d26b06de08fa35e9de155f0ca70f
Jack Rose <user@jackrose.co.nz>
2022-08-01 21:01:57 +1000

    Revert "many things" so I can run through tutorial properly
    This reverts commit 85b19dfc5d3d9468712f00aae98dadd8fcc4e0ea.

--- lambda/hello.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (event) {
-  console.log("request:", JSON.stringify(event, null, 2));
-  return {
-    statusCode: 200,
-    headers: { "Content-type": "text/plain" },
-    body: `Hello, CDK! You've hit ${process.env.DOWNSTREAM_FUNCTION_NAME}\n`,
-  };
-};
